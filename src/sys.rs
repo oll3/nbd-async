bcce9571f089ed9560f3393ecf6ee6a40d50dc4f
Olle Sandberg <olle@b3rg.se>
2021-09-20 16:17:56 +0200

    chore: bump nix dependency

--- src/sys.rs
+++ src/sys.rs
@@ -25,75 +25,79 @@ pub fn set_sock<F>(f: &F, sock: RawFd) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 0), sock) })
-        .map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 0), sock)
+    })?)
 }
 
 pub fn set_block_size<F>(f: &F, size: u32) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 1), size) })
-        .map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 1), size)
+    })?)
 }
 
 pub fn do_it<F>(f: &F) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 3)) }).map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 3))
+    })?)
 }
 
 pub fn clear_sock<F>(f: &F) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 4)) }).map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 4))
+    })?)
 }
 
 pub fn clear_queue<F>(f: &F) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 5)) }).map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 5))
+    })?)
 }
 
 pub fn set_size_blocks<F>(f: &F, size: u64) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 7), size) })
-        .map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 7), size)
+    })?)
 }
 
 pub fn disconnect<F>(f: &F) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 8)) }).map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 8))
+    })?)
 }
 
 pub fn set_timeout<F>(f: &F, timeout: u64) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 9), timeout) })
-        .map_err(errno_to_io)
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 9), timeout)
+    })?)
 }
 
 pub fn set_flags<F>(f: &F, flags: u64) -> io::Result<i32>
 where
     F: AsRawFd,
 {
-    Errno::result(unsafe { ioctl(f.as_raw_fd(), request_code_none!(0xab, 10), flags) })
-        .map_err(errno_to_io)
-}
-
-fn errno_to_io(error: nix::Error) -> io::Error {
-    match error {
-        nix::Error::Sys(errno) => io::Error::from_raw_os_error(errno as i32),
-        nix::Error::InvalidPath => io::Error::from(io::ErrorKind::InvalidInput),
-        nix::Error::InvalidUtf8 => io::Error::from(io::ErrorKind::InvalidData),
-        nix::Error::UnsupportedOperation => io::Error::new(io::ErrorKind::Other, "not supported"),
-    }
+    Ok(Errno::result(unsafe {
+        ioctl(f.as_raw_fd(), request_code_none!(0xab, 10), flags)
+    })?)
 }
